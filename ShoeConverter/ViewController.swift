c263fa3cd7f8b1c5233f2e2422ed450b75a1f971
Cade Ward <cadew@live.com>
2014-09-19 19:36:18 -0600

    messing with swift conversions

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtMenShoeSize: UITextField!
+    
+    @IBOutlet weak var lblConvertedShoeSize: UILabel!
+    
+    @IBOutlet weak var txtWomenShoeSize: UITextField!
+    
+    @IBOutlet weak var lblConvertedWomenShoeSize: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnConvert_TouchUpInside(sender: UIButton) {
+        lblConvertedShoeSize.hidden = false
+        lblConvertedShoeSize.text = "\(txtMenShoeSize.text.toInt()! + 30) in European Shoe Size"
+    }
 
+    @IBAction func btnConvertWomen_TouchUpInside(sender: AnyObject) {
+        let sizeFromTextField = Double((txtWomenShoeSize.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        lblConvertedWomenShoeSize.hidden = false
+        lblConvertedWomenShoeSize.text = "\(sizeFromTextField + conversionConstant) in European Shoe Size"
+    }
 }
 
